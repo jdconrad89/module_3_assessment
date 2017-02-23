518782c4caa503465a8d0dd44cc9a84f7d76d907
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-02-23 10:24:08 -0700

    added distance to to my api call and spent time researching how get pieces of json to show in the index page

--- app/services/store_service.rb
+++ app/services/store_service.rb
@@ -1,7 +1,7 @@
 class StoreService
 
   def self.find_by_zipcode(zipcode)
-    conn = Faraday.get("https://api.bestbuy.com/v1/stores((area(80202,25)))?apiKey=#{ENV["api_key"]}&show=longName,city,phone,storeType&format=json")
+    conn = Faraday.get("https://api.bestbuy.com/v1/stores((area(80202,25)))?apiKey=#{ENV["api_key"]}&show=longName,city,distance,phone,storeType&format=json")
     response = JSON.parse(conn.body, symoblize_names: true)
   end
 end
