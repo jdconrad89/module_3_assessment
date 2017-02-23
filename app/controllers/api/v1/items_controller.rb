05a1737d94e3f9aec892c1d16c6abc0e8feae142
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-02-23 11:47:37 -0700

    add passing test with functionality for creating an item in the api

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -13,4 +13,19 @@ class Api::V1::ItemsController < ApplicationController
     item.delete
     render json: Item.all
   end
+
+  def create
+    item = Item.new(item_params)
+
+    if item.save
+      render json: item
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :image_url)
+  end
+
 end
--- spec/api/v1/requests/items_requests_spec.rb
+++ spec/api/v1/requests/items_requests_spec.rb
@@ -25,7 +25,7 @@ describe "items endpoint" do
       item2 = Item.create!(name: "Turing2", description: "Be prepared", image_url: "Image2")
       item3 = Item.create!(name: "Turing3", description: "It will change your life", image_url: "Image3")
 
-      get "api/v1/items/1"
+      get "/api/v1/items/1"
 
       expect(response).to be_success
       item = JSON.parse(response.body)
@@ -51,7 +51,7 @@ describe "items endpoint" do
       expect(items.first["description"]).to eq("Best School")
       expect(items.first["image_url"]).to eq("Image1")
 
-      delete "api/v1/items/1"
+      delete "/api/v1/items/1"
       expect(response).to be_success
 
 
@@ -59,6 +59,28 @@ describe "items endpoint" do
       expect(response).not_to include("Turing1")
       expect(response).not_to include("Best School")
       expect(response).not_to include("Image1")
+    end
+  end
+
+  context "POST an item by ID" do
+    it "can create an item" do
+      item1 = Item.create!(name: "Turing1", description: "Best School", image_url: "Image1")
+      item2 = Item.create!(name: "Turing2", description: "Be prepared", image_url: "Image2")
+      item3 = Item.create!(name: "Turing3", description: "It will change your life", image_url: "Image3")
+
+
+      post '/api/v1/items/', item: {:name => "Turing4", :description => "Let's Do This!", :image_url => "Image4" }
+
+      expect(response).to be_success
+
+      get "/api/v1/items/#{Item.last.id}"
+      item = JSON.parse(response.body)
+
+      expect(response).to be_success
+
+      expect(item["name"]).to eq("Turing4")
+      expect(item["description"]).to eq("Let's Do This!")
+      expect(item["image_url"]).to eq("Image4")
     end
   end
 end
